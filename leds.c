4c836af00a0ee8500affd368fe9fc8ec2098a5ad
Jeremy Jones <jeremy.jones@cazoodle.com>
2011-03-14 01:26:26 -0500

    added MSP430 LED/IRQ test

---
+++ leds.c
@@ -0,0 +1,66 @@
+#include  <msp430g2231.h>
+
+/* Global for delay timer */
+int delay_multiplyer = 5;
+
+void main(void)
+{
+    unsigned int i;
+
+    WDTCTL = WDTPW + WDTHOLD;  // Stop watchdog timer
+    P1DIR = 0;
+    P1DIR = BIT0|BIT6; // Set LEDs to out
+    
+    /* Turn LED1 on and LED2 off */
+    P1OUT |= BIT0;
+    P1OUT &= ~BIT6;
+    
+    /* Stole this from somewhere.  Seems to be right for the onboard switch */
+    P1IES |= BIT3;   // high -> low is selected with IES.x = 1.
+    
+    
+    /* Clear the irq flag for P1.3, so we don't throw an immediate irq
+     * Then turn on IRQs
+     */
+    P1IFG &= ~BIT3;
+    P1IE |= BIT3;
+    
+    _enable_interrupt(); // umm, what it says
+
+        for (;;){
+            /* modified from some example */
+            i = 10000*delay_multiplyer;
+            do (i--);
+            while (i != 0);
+            
+            /* flip the LEDs */
+            P1OUT ^= BIT0;
+            P1OUT ^= BIT6;
+    }
+} 
+
+
+/* IRQ handler on GPIO port 1 */
+#pragma vector = PORT1_VECTOR
+__interrupt void P1_ISR(void) {
+    switch(P1IFG&BIT3) {
+        case BIT3:
+            P1IFG &= ~BIT3; // clear
+            if (delay_multiplyer == 0){
+                delay_multiplyer = 5;
+            }
+            else {
+                delay_multiplyer--;
+            }
+            return;
+        default:
+             /* clear all flags -->
+              * This can cause lost IRQs, but we only have one input, so 
+              * it's a non-issue here.
+              */
+            P1IFG = 0;
+            return;
+    }
+}
+
+
