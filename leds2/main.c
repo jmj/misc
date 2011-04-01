674e5b9ad96cda998fd84cefae0e70cb709f87cd
Jeremy Jones <jeremyj@letifer.org>
2011-03-31 21:05:03 -0500

    rewrite the entire thing to use timer interrupts

--- leds2/main.c
+++ leds2/main.c
@@ -6,9 +6,12 @@
 #include  <msp430g2231.h>
 #endif
 
+#define DELAY 600
 /* Global for delay timer */
 volatile int delay_multiplyer = 5;
 volatile int dir = 1;
+volatile int dir2 = 1;
+volatile int mode = 0;
 
 /*
  * basic init function.  Use this for almost every project
@@ -33,11 +36,6 @@ void init_board(int button_enable) {
 
 int main(void)
 {
-	unsigned long i;
-	int mode = 0;
-	int dir2 = 1;
-
-	
 	init_board(1);
 	
 	P1IES |= BIT3;   // high -> low is selected with IES.x = 1.
@@ -58,7 +56,7 @@ int main(void)
     BCSCTL3 |= LFXT1S_2;
     TACTL = TASSEL_1 | MC_1;
     TACCTL0 = CCIE;
-    TACCR0 = 2400 * delay_multiplyer;
+    TACCR0 = DELAY * delay_multiplyer;
 
     while(1){
         //nothing
@@ -71,7 +69,40 @@ interrupt(TIMERA0_VECTOR) TIMERA0_ISR(void) {
 #pragma vector = TIMERA0_VECTOR
 __interrupt void TIMERA0_ISR(void) {
 #endif
-    P1OUT ^= BIT0;
+
+    P1OUT = 0;
+    switch (mode%5) {
+        case 0:
+            P1OUT |= BIT0;
+            break;
+        case 1:
+            P1OUT |= BIT4;
+            break;
+        case 2:
+            P1OUT |= BIT5;
+            break;
+        case 3:
+            P1OUT |= BIT6;
+            break;
+        case 4:
+            P1OUT |= BIT7;
+            break;
+    }
+
+    if (dir2) {
+        mode++;
+        if (mode == 4) {
+            dir2 = 0;
+        }
+    }
+    else {
+        mode--;
+        if (mode == 0) {
+            dir2 = 1;
+        }
+    }
+
+//    P1OUT ^= BIT0;
 }
 
 #ifdef __GNUC__
@@ -83,7 +114,18 @@ __interrupt void P1_ISR(void) {
     switch(P1IFG&BIT3) {
         case BIT3:
             P1IFG &= ~BIT3; // clear
-            TACCR0 = 2400 * delay_multiplyer--;
+            if (dir) {
+                TACCR0 = DELAY * delay_multiplyer--;
+                if (delay_multiplyer == 1) {
+                    dir = 0;
+                }
+            }
+            else {
+                TACCR0 = DELAY * delay_multiplyer++;
+                if (delay_multiplyer == 5) {
+                    dir = 1;
+                }
+            }
             return;
         default:
             P1IFG = 0;
