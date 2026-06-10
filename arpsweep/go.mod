1225073435519d8e4ecff97d42e2f0774d6ce7e2
Jeremy Jones <jeremyj@letifer.org>
2026-06-10 17:07:40 -0600

    prelim go version

---
+++ arpsweep/go.mod
@@ -0,0 +1,15 @@
+module letifer.org/goarp
+
+go 1.26.2
+
+require (
+	github.com/3th1nk/cidr v0.3.0 // indirect
+	github.com/josharian/native v1.1.0 // indirect
+	github.com/mdlayher/arp v0.0.0-20260528070854-93566ba168e9 // indirect
+	github.com/mdlayher/ethernet v0.0.0-20220221185849-529eae5b6118 // indirect
+	github.com/mdlayher/packet v1.1.2 // indirect
+	github.com/mdlayher/socket v0.4.1 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+)
---
+++ arpsweep/goarp.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	arp "github.com/mdlayher/arp"
+	"github.com/3th1nk/cidr"
+	"net/netip"
+	"net"
+	"time"
+//	"os"
+//	"errors"
+)
+
+var testip = "192.168.1.1"
+var testcidr = "192.168.1.0/24"
+
+func main() {
+
+	netif, err := net.InterfaceByName("wlp0s20f3")
+	if err != nil {
+		panic(err)
+	}
+
+	cidr, err := cidr.Parse(testcidr)
+	if err != nil {
+		panic(err)
+	}
+	c, err := arp.Dial(netif)
+	if err != nil {
+		panic(err)
+	}
+	defer c.Close()
+
+
+	cidr.Each(func(ip string) bool {
+		pip := net.ParseIP(ip)
+		if pip.Equal(cidr.Network()) {
+			return true
+		}
+		if pip.Equal(cidr.EndIP()) {
+			//fmt.Printf("\n")
+			return false
+		}
+
+		ipaddr, err := netip.ParseAddr(ip)
+		if err != nil {
+			panic(err)
+		}
+
+		//c.SetDeadline(time.Now().Add(time.Millisecond*300))
+		c.SetDeadline(time.Now().Add(time.Millisecond*100))
+
+		fmt.Printf("%s: ", ip)
+		hw, err := c.Resolve(ipaddr)
+		if err != nil {
+			//panic(err)
+			//if (!errors.Is(err, os.ErrDeadlineExceeded)) {
+			//	panic(err)
+			//}
+			//fmt.Printf("\r")
+			fmt.Print("\033[2K\r")
+			return true
+		}
+
+		fmt.Printf("%s\n", hw.String())
+
+		return true
+	})
+}
